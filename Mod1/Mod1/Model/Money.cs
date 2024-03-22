d6bb51dec44cea4db21ea1a8b7bc4c05139bdc27
gayashan4lk <gayashan4lk@gmail.com>
2024-03-22 21:18:16 +0530

    Add project files.

---
+++ Mod1/Mod1/Model/Money.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1.Model;
+
+public class Money
+{
+    public Money(string isoCurrency, decimal amount)
+    {
+        isoCurrency = isoCurrency;
+        amount = amount;
+    }
+
+    public string IsoCurrency { get; set; }
+    public decimal Amount { get; set;}
+}
